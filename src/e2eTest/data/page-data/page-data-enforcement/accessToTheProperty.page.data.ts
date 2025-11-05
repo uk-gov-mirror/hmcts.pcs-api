92b86eee41d00d51a6c28554aaecdd4bdf9e2b6b
“Kunchhal <ankita.kunchhal@hmcts.net>
2025-11-05 10:16:38 +0000

    formatting fix

--- src/e2eTest/data/page-data/page-data-enforcement/accessToTheProperty.page.data.ts
+++ src/e2eTest/data/page-data/page-data-enforcement/accessToTheProperty.page.data.ts
@@ -1,10 +1,10 @@
 export const accessToTheProperty = {
-  title: 'Create a case - HM Courts & Tribunals Service - GOV.UK',
-  mainHeader: 'Access to the property',
-  accessQuestion: 'Is it difficult to access the property?',
-  yes: 'Yes',
-  labelExplainWhyItsDifficultToAccessTheProperty: 'Explain why its difficult to access the property',
-inputExplainWhyItsDifficultToAccessTheProperty: 'ExplainWhyItsDifficultToAccessThePropertyinput',
-  no: 'No',
-  continue: 'Continue',
+    title: 'Create a case - HM Courts & Tribunals Service - GOV.UK',
+    mainHeader: 'Access to the property',
+    accessQuestion: 'Is it difficult to access the property?',
+    yes: 'Yes',
+    labelExplainWhyItsDifficultToAccessTheProperty: 'Explain why its difficult to access the property',
+    inputExplainWhyItsDifficultToAccessTheProperty: 'ExplainWhyItsDifficultToAccessThePropertyinput',
+    no: 'No',
+    continue: 'Continue',
 }
